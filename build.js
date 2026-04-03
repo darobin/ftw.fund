40cf1b5ee8556c0107d2ee5fb25136e8a5a3fbf9
Robin Berjon <robin.berjon@nytimes.com>
2026-04-03 18:44:50 +0200

    more gen

--- build.js
+++ build.js
@@ -22,11 +22,15 @@ const indexData = await readFile(index, 'utf8');
 // Exec Summary
 const exec = [...report.window.document.querySelectorAll('h1')].find(h => h.textContent === 'Executive Summary').parentElement.cloneNode(true);
 exec.querySelector('h1').remove();
-const indexOut = indexData.replace(/<!--EXECUTIVE-->.*<!--\/EXECUTIVE-->/, `<!--EXECUTIVE-->${exec.innerHTML}<!--/EXECUTIVE-->`);
+const indexOut = indexData.replace(/<!--EXECUTIVE-->[\s\S]*<!--\/EXECUTIVE-->/, `<!--EXECUTIVE-->${exec.innerHTML}<!--/EXECUTIVE-->`);
 await writeFile(index, indexOut, 'utf8');
 
-// - add style to copied HTML (maybe other cleanup?)
-// - put the Exec Summary in the index.html
+// Add style to report
+//  - link to report CSS
+//  - wrap in main
+//  - header > p.title to h1
+//  - need to copy some images over
+//  - hX + 1 & remove ID
 
 function rel(pth) {
   return new URL(pth, import.meta.url).toString().replace(/^file:\/\//, '');
